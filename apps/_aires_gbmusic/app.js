af605f532a728c72ee49bb4b2b3a7a70b43e4163
Andre <andre@8bitbuddhism.com>
2025-05-31 12:09:35 -0400

    It's aliiiiive!

--- apps/_aires_gbmusic/app.js
+++ apps/_aires_gbmusic/app.js
@@ -1,5 +1,5 @@
 // For info on interfacing with Gadgetbridge, see https://www.espruino.com/Gadgetbridge
-const AppName = "_aires_gbmusic.app.js";
+const AppName = "_aires_gbmusic";
 const Debug = false;  // Set to true to show debugging into
 const Layout = require("Layout");
 const Storage = require("Storage");
@@ -17,22 +17,25 @@ const PlaybackState = {
   playing: "playing"
 };
 
-// Global playback state tracker.
-// Follows the syntax {t:"musicstate", state:"play/pause",position,shuffle,repeat}
-
+/**
+ * Global playback state tracker.
+ * Follows the syntax {t:"musicstate", state:"play/pause",position,shuffle,repeat}
+ */
 let appState = {t:"musicstate", state:"pause", position:0, shuffle:0, repeat:0};
 
 /**
- * Send a command via Bluetooth back to Gadgetbridge.
- * @param {"play"|"pause"|"next"|"previous"} command
+ * Define the screen layout.
  */
-function sendCommand(command) {
-  Bluetooth.println(JSON.stringify({t: "music", n: command}));
-
-  // If this is a play or pause command, update the app state
-  if (command === Command.play) appState = PlaybackState.playing;
-  if (command === Command.pause) appState = PlaybackState.paused;
-}
+let layout = new Layout({
+  type: "v", c: [
+    {type: "txt", id: "title", font:PrimaryFont, label: "Unknown Track"},
+    {type: "h", c: [
+      {type: "btn", id: Command.previous, font: PrimaryFont, label: "|<<", cb: l=>sendCommand(Command.previous)},
+      {type: "btn", id: "playpause", font: PrimaryFont, label: ">||", cb: l=>sendCommand(appState.state === PlaybackState.paused ? Command.play : Command.pause)},
+      {type: "btn", id: Command.next, font: PrimaryFont, label: ">>|", cb: l=>sendCommand(Command.next)}
+    ]},
+  ]
+}, {lazy: true});
 
 /**
  * Detect whether we're using an emulator.
@@ -43,27 +46,6 @@ function detectEmulator() {
       println: (line) => {console.log("Bluetooth:", line);},
     };
   }
-}
-
-var layout = new Layout({
-  type: "v", c: [
-    {type: "txt", id: "title", font:PrimaryFont, label: "Unknown Track"},
-    {type: "h", c: [
-      {type: "btn", id: Command.previous, font: PrimaryFont, label: "|<<", cb: l=>sendCommand(Command.previous)},
-      {type: "btn", id: "playpause", font: PrimaryFont, label: ">||", cb: l=>sendCommand(appState == PlaybackState.paused ? Command.play : Command.pause)},
-      {type: "btn", id: Command.next, font: PrimaryFont, label: ">>|", cb: l=>sendCommand(Command.next)}
-    ]},
-  ]
-}, {lazy: true});
-
-/**
- * Get info about the current playing song.
- * @param {Object} info - Gadgetbridge musicinfo event
- */
-function showTrackInfo(info) {
-  layout.title.label = info ? info.track : "Unknown Track";
-  layout.render();
-  if (Debug) layout.debug();
 }
 
 /**
@@ -80,13 +62,75 @@ function loadFromStorage() {
 }
 
 /**
+ * Send a command via Bluetooth back to Gadgetbridge.
+ * @param {"play"|"pause"|"next"|"previous"} command
+ */
+function sendCommand(command) {
+  Bluetooth.println(JSON.stringify({t: "music", n: command}));
+
+  // If this is a play or pause command, update the app state
+  if (command === Command.play) updateState(PlaybackState.playing);
+  if (command === Command.pause) updateState(PlaybackState.paused);
+}
+
+/**
+ * Get info about the current playing song.
+ * Gets called from boot.js too.
+ * @param {Object} info - Gadgetbridge musicinfo event
+ */
+function showTrackInfo(info) {
+  layout.title.label = info ? info.track : "Unknown Track";
+  layout.render();
+  if (Debug) layout.debug();
+}
+
+/**
  * Updates the current state of the app.
  * Called when Gadgetbridge updates (see boot.js)
  * @param {*} state 
  */
 function updateState(state) {
-  appState = state;
+  appState.state = state;
 }
+
+/**
+ * Listen for Gadgetbridge events
+ */
+setTimeout( // make other boot code run first
+  () => {
+    let state, info;
+ 
+    /**
+     * Store track info so the app can grab it later
+     */
+    function saveTrackInfo() {
+      require("Storage").writeJSON(`${AppName}.load.json`, {
+        state: state,
+        info: info,
+      });
+    }
+
+    /**
+     * Check to see if we're currently running the app.
+     * If so, send music events to the app.
+     * If not, save music events for later.
+     */
+    const IsAppOpen = globalThis.__FILE__ === `${AppName}.app.js`;
+    globalThis.GB = (_GB => e => {
+      switch(e.t) {
+        case "musicinfo":
+          info = e;
+          return IsAppOpen ? globalThis.showTrackInfo(e) : saveTrackInfo();
+        case "musicstate":
+          state = e;
+          return IsAppOpen ? globalThis.updateState(e) : saveTrackInfo();
+        default:
+          // pass on other events
+          if (_GB) setTimeout(_GB, 0, e);
+      }
+    })(globalThis.GB);
+  }, 1);
+
 
 // Start the app
 detectEmulator();
--- apps/_aires_gbmusic/boot.js
+++ apps/_aires_gbmusic/boot.js
@@ -1,30 +1,33 @@
-const AppName = "_aires_gbmusic";
-setTimeout( // make other boot code run first, so we override e.g. android.boot.js GB
+setTimeout( // make other boot code run first
   () => {
-    const APP = globalThis.__FILE__=== `${AppName}.app.js`,
-      a = !!(require("Storage").readJSON(`${AppName}.json`, 1) || {}).autoStart;
+    const AppName = require("_aires_gbmusic.metadata.json").id;
 
     let state, info;
  
     /**
      * Store track info so the app can grab it later
      */
-    function getTrackInfo() {
-        require("Storage").writeJSON(`${AppName}.load.json`, {
+    function saveTrackInfo() {
+      require("Storage").writeJSON(`${AppName}.load.json`, {
         state: state,
         info: info,
       });
     }
 
+    /**
+     * Check to see if we're currently running the app.
+     * If so, send music events to the app.
+     * If not, save music events for later.
+     */
+    const IsAppOpen = globalThis.__FILE__ === `${AppName}.app.js`;
     globalThis.GB = (_GB => e => {
-      // we eat music events!
       switch(e.t) {
         case "musicinfo":
           info = e;
-          return APP ? globalThis.showTrackInfo(e) : getTrackInfo();
+          return IsAppOpen ? globalThis.showTrackInfo(e) : saveTrackInfo();
         case "musicstate":
           state = e;
-          return APP ? globalThis.updateState(e) : getTrackInfo();
+          return IsAppOpen ? globalThis.updateState(e) : saveTrackInfo();
         default:
           // pass on other events
           if (_GB) setTimeout(_GB, 0, e);
